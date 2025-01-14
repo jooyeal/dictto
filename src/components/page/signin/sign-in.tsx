fdb468bb6553fa2f782498cafd536ddffaa1d4de
jooyeal <34053954+jooyeal@users.noreply.github.com>
2025-01-14 22:16:23 +0900

    ADD: Google sign-in

--- src/components/page/signin/sign-in.tsx
+++ src/components/page/signin/sign-in.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 export default function Signin() {
-  return <div></div>;
+  return (
+    <div>
+      <Button onClick={() => signIn()}>signin</Button>
+    </div>
+  );
 }
